327270b2cf10e351bbdb607f392aa51cf9e811d0
Nicolai Mollerup <nicolai@mollerup.me>
2024-12-20 14:59:15 +0100

    first checkin

---
+++ cmd/check-postgres-alive/main.go
@@ -0,0 +1,137 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx/v5"
+	corev2 "github.com/sensu/sensu-go/api/core/v2"
+	"github.com/sensu/sensu-plugin-sdk/sensu"
+)
+
+type Config struct {
+	sensu.PluginConfig
+	User     string
+	Password string
+	IniFile  string
+	Hostname string
+	Port     uint
+	Database string
+	Sslmode  string
+}
+
+var (
+	plugin = Config{
+		PluginConfig: sensu.PluginConfig{
+			Name:     "check-postgres-alive",
+			Short:    "postgres alive check",
+			Keyspace: "",
+		},
+	}
+
+	options = []sensu.ConfigOption{
+		&sensu.PluginConfigOption[string]{
+			Path:      "User",
+			Argument:  "user",
+			Shorthand: "u",
+			Usage:     "postgres user to connect",
+			Value:     &plugin.User,
+		},
+		&sensu.PluginConfigOption[string]{
+			Path:      "Password",
+			Argument:  "password",
+			Shorthand: "p",
+			Usage:     "Password for user",
+			Value:     &plugin.Password,
+		},
+		&sensu.PluginConfigOption[string]{
+			Path:      "pgpass file",
+			Argument:  "pgpass",
+			Shorthand: "f",
+			Usage:     "Location of .pgpass file for access to postgres",
+			Value:     &plugin.IniFile,
+		},
+		&sensu.PluginConfigOption[uint]{
+			Path:     "port",
+			Argument: "port",
+			Usage:    "Port to connect to",
+			Value:    &plugin.Port,
+			Default:  5432,
+		},
+		&sensu.PluginConfigOption[string]{
+			Path:     "hostname",
+			Argument: "hostname",
+			Usage:    "Hostname to login to",
+			Value:    &plugin.Hostname,
+			Default:  "localhost",
+		},
+		&sensu.PluginConfigOption[string]{
+			Path:      "database",
+			Argument:  "database",
+			Shorthand: "d",
+			Usage:     "Database schema to connect to",
+			Value:     &plugin.Database,
+			Default:   "postgres",
+		},
+		&sensu.PluginConfigOption[string]{
+			Path:      "sslmode",
+			Argument:  "sslmode",
+			Shorthand: "s",
+			Usage:     "SSL mode for connecting to postgres",
+			Value:     &plugin.Sslmode,
+			Default:   "prefer",
+		},
+	}
+)
+
+func main() {
+	check := sensu.NewCheck(&plugin.PluginConfig, options, checkArgs, executeCheck, false)
+	check.Execute()
+}
+
+func checkArgs(event *corev2.Event) (int, error) {
+	if plugin.Port <= 1 || plugin.Port >= 65535 {
+		return sensu.CheckStateCritical, fmt.Errorf("invalid port, should be a value between 1 and 65535")
+	}
+	if plugin.IniFile != "" {
+		if _, err := os.Stat(plugin.IniFile); os.IsNotExist(err) {
+			return sensu.CheckStateCritical, fmt.Errorf("unable to open the supplied config file %s", plugin.IniFile)
+		}
+	}
+
+	return sensu.CheckStateOK, nil
+}
+
+func executeCheck(event *corev2.Event) (int, error) {
+	var dataSourceName string
+
+	var dbUser, dbPass string
+	if plugin.IniFile != "" {
+		//iniFile, err := ini.Load(plugin.IniFile)
+		//if err != nil {
+		//	return sensu.CheckStateCritical, fmt.Errorf("error parsing ini file: %v", err)
+		//}
+		// dbUser = iniFile.Section(plugin.IniSection).Key("user").String()
+		//dbPass = iniFile.Section(plugin.IniSection).Key("password").String()
+	} else {
+		dbUser = plugin.User
+		dbPass = plugin.Password
+	}
+
+	dataSourceName = fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=%s", dbUser, dbPass, plugin.Hostname, plugin.Port, plugin.Database, plugin.Sslmode)
+	fmt.Print(dataSourceName)
+	db, err := pgx.Connect(context.Background(), dataSourceName)
+	if err != nil {
+		return sensu.CheckStateCritical, fmt.Errorf("error connecting to postgres: %v", err)
+	}
+	defer db.Close(context.Background())
+
+	err = db.Ping(context.Background())
+	if err != nil {
+		return sensu.CheckStateCritical, fmt.Errorf("error pinging postgres: %v", err)
+	}
+
+	fmt.Printf("postgres server is alive.")
+	return sensu.CheckStateOK, nil
+}
---
+++ go.mod
@@ -0,0 +1,52 @@
+module nmollerup/sensu-check-postgres
+
+go 1.22.2
+
+require (
+	github.com/jackc/pgx/v5 v5.7.1
+	github.com/sensu/sensu-go/api/core/v2 v2.14.0
+	github.com/sensu/sensu-plugin-sdk v0.16.0
+)
+
+require (
+	github.com/blang/semver/v4 v4.0.0 // indirect
+	github.com/coreos/go-semver v0.3.0 // indirect
+	github.com/echlebek/timeproxy v1.0.0 // indirect
+	github.com/fsnotify/fsnotify v1.4.7 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang-jwt/jwt/v4 v4.4.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/magiconair/properties v1.8.1 // indirect
+	github.com/mitchellh/mapstructure v1.1.2 // indirect
+	github.com/pelletier/go-toml v1.2.0 // indirect
+	github.com/robertkrimen/otto v0.0.0-20221006114523-201ab5b34f52 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
+	github.com/rogpeppe/go-internal v1.13.1 // indirect
+	github.com/sensu/sensu-go/types v0.10.0 // indirect
+	github.com/sensu/sensu-licensing v0.1.2 // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
+	github.com/spf13/afero v1.1.2 // indirect
+	github.com/spf13/cast v1.3.0 // indirect
+	github.com/spf13/cobra v1.4.0 // indirect
+	github.com/spf13/jwalterweatherman v1.0.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.7.0 // indirect
+	github.com/subosito/gotenv v1.2.0 // indirect
+	go.etcd.io/etcd/api/v3 v3.5.5 // indirect
+	golang.org/x/crypto v0.27.0 // indirect
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
+	google.golang.org/genproto v0.0.0-20230110181048-76db0878b65f // indirect
+	google.golang.org/grpc v1.53.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/sourcemap.v1 v1.0.5 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
